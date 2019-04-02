3d3dda9c4e427df8f791b5ddb2695f62645756e2
Vladislav <Vladislav@10.0.2.15>
2019-04-02 17:25:49 +0200

    terzo commit prova

--- ProvaGIT/src/ProvaVlad.java
+++ ProvaGIT/src/ProvaVlad.java
@@ -3,7 +3,7 @@ public class ProvaVlad {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("Ciao 123 prova");
+System.out.println("Hello world");
 	}
 
 }
